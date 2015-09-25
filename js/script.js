fe0193c3a323312757187fe6624279d766c0ea5e
lzzk <1259367356@qq.com>
2015-09-25 14:35:13 +0800

    Site updated: 2015-09-25 14:35:12

--- js/script.js
+++ js/script.js
@@ -69,8 +69,6 @@
 
     if (type == 'baidu') {
       var box = $('#article-share-box');
-      shareDataUrl = $this.attr('data-url');
-      shareDataTitle = $this.attr('data-title');
 
       if (box.hasClass('on')){
         box.removeClass('on');
